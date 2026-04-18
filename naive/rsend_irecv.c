f1a92e91a29a857e6483dffe068ee774eac9cf49
PCBMoreira0 <pcbmoreira@id.uff.br>
2026-04-18 14:27:36 -0300

    rsend

---
+++ naive/rsend_irecv.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <mpi.h>
+
+int primo(long int n) {
+	int i;
+       
+	for (i = 3; i < (int)(sqrt(n) + 1); i+=2) {
+		if(n%i == 0) return 0;
+	}
+	return 1;
+}
+
+int main(int argc, char *argv[]) {
+    double t_inicial, t_final;
+    int cont = 0, total = 0;
+    long int i, n;
+    int meu_ranque, num_procs, inicio, salto;
+    
+    if (argc < 2) {
+        printf("Valor inválido!\n");
+        return 0;
+    } else {
+        n = strtol(argv[1], (char **) NULL, 10);
+    }
+
+    MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &meu_ranque);
+	MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
+
+    MPI_Request requests[num_procs-1];
+    int results[num_procs-1];
+
+    for(i = 1; i < num_procs; i++){
+        MPI_Request request;
+        MPI_Irecv(&results[i-1], 1, MPI_INT, i, 0, MPI_COMM_WORLD, &requests[i-1]);
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    t_inicial = MPI_Wtime();
+    inicio = 3 + meu_ranque*2;
+    salto = num_procs*2;
+	for (i = inicio; i <= n; i += salto) {
+		if(primo(i) == 1) cont++;
+    }
+
+    if(num_procs > 1){
+        if(meu_ranque == 0){
+            MPI_Waitall(num_procs-1, requests, MPI_STATUS_IGNORE);
+            for(i = 0; i < num_procs-1; i++){
+                cont += results[i];
+            }
+        }
+        else{
+            MPI_Rsend(&cont, 1 , MPI_INT, 0, 0, MPI_COMM_WORLD);
+        }
+    }
+
+    t_final = MPI_Wtime();
+    if(meu_ranque == 0){
+        cont++;
+        printf("Total de primos: %d\n", cont);
+        printf("Tempo: %f\n", t_final - t_inicial);
+    }
+
+    MPI_Finalize();
+    return 0;
+}
